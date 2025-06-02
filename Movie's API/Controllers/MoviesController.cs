8fe9e3c263d197cb52f1747cc875739702e7ce63
jaime1127 <84351799+jaime1127@users.noreply.github.com>
2025-06-02 12:18:56 -0400

    controller added for movies

---
+++ Movie's API/Controllers/MoviesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Movies.Application.Repositories;
+
+namespace Movie_s_API.Controllers
+{
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly IMovieRepository _movieRepository;
+
+        public MoviesController(IMovieRepository movieRepository)
+        {
+            _movieRepository = movieRepository;
+        }
+    }
+}
